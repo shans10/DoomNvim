94916d9bd3e9981c4205338e172de0b6e49c09fd
Shantanu Shukla <shantanu.shukla10@gmail.com>
2022-07-15 17:46:01 +0530

    modify current indent highlight

--- lua/default_theme/plugins/indent_blankline.lua
+++ lua/default_theme/plugins/indent_blankline.lua
@@ -2,6 +2,6 @@ return {
   IndentBlanklineSpaceChar = { fg = C.grey_6, nocombine = true },
   IndentBlanklineChar = { fg = C.grey_6, nocombine = true },
   IndentBlanklineContextStart = { underline = true },
-  IndentBlanklineContextChar = { fg = C.cyan, nocombine = true },
+  IndentBlanklineContextChar = { fg = C.grey_9, nocombine = true },
   IndentBlanklineSpaceCharBlankline = { fg = C.grey_6, nocombine = true },
 }
